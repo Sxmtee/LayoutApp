9f5f1b67ea3749f34d3fbb4f033f6f97a60f2c39
sxmtee <somtoo76@gmail.com>
2024-11-29 10:33:13 +0100

    Initial Commit

---
+++ LayoutApp/LayoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAppApp.swift
+//  LayoutApp
+//
+//  Created by mac on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
